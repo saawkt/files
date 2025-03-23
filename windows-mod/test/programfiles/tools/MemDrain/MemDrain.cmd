72188697d12f8f5deb44a84bd80d8bdf79f676bd
wasynxyxxk <202036399+wasynxyxxk@users.noreply.github.com>
2025-03-23 00:12:02 -0300

    1.0.1
    add memdrain from LuSlower

---
+++ windows-mod/test/programfiles/tools/MemDrain/MemDrain.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Program Files\tools\NSudo
+NSudoLG.exe -U:T -ShowWindowMode:Hide C:\Program Files\tools\MemDrain\md.cmd
+exit /b
---
+++ windows-mod/test/programfiles/tools/MemDrain/md.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Program Files\tools\MemDrain
+md.exe -all
+exit /b
