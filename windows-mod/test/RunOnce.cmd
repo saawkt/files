a0a15f3414f456fe5c8fa0ddf053a6710c3c98a6
wasynxyxxk <202036399+wasynxyxxk@users.noreply.github.com>
2025-03-23 22:09:57 -0300

    1.0.2
    fix xhci, add dwmenablemmcss

--- windows-mod/test/RunOnce.cmd
+++ windows-mod/test/RunOnce.cmd
@@ -736,7 +736,7 @@ powercfg /hibernate off
 :: BlitzOS Script (Spectre meltdown)
 wmic cpu get name | findstr "Intel" >nul && (
     reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d 3 /f
-move "C:\bin\3\XHCI-IMOD-Interval.ps1" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\StartUp"
+move "C:\bin\3\xhci.cmd" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\StartUp"
 )
 wmic cpu get name | findstr "AMD" >nul && (
     reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d 64 /f
