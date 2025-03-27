64b59d982bda8ea1dd9cc89001326d37e7d6378c
wasynxyxxk <202036399+wasynxyxxk@users.noreply.github.com>
2025-03-26 22:22:30 -0300

    remove post install

--- windows-mod/test/Desktop/Post-setup/5 - GPU/nvidia/Disable NvContainer (breaks nvcp).bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Disabling NVIDIA Control Panel Service
-
-sc config NVDisplay.ContainerLocalSystem start= disabled >NUL 2>&1
-sc stop NVDisplay.ContainerLocalSystem >NUL 2>&1
--- windows-mod/test/Desktop/Post-setup/5 - GPU/nvidia/Enable NvContainer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Enabling NVIDIA Control Panel Service
-
-sc config NVDisplay.ContainerLocalSystem start= auto >NUL 2>&1
-sc start NVDisplay.ContainerLocalSystem >NUL 2>&1
