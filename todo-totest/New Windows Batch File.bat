41bf11cd08c67d1a3524429e4b3e50ddcfd1398f
wasynxyxxk <202036399+saawkt@users.noreply.github.com>
2025-06-02 13:42:00 -0300

    update readme

--- todo-totest/New Windows Batch File.bat
+++
@@ -1,5 +0,0 @@
-Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class%%i" /v "RMElcg" /t REG_DWORD /d "55555555" /f > nul 2>&1
-Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class%%i" /v "RMBlcg" /t REG_DWORD /d "1111111" /f > nul 2>&1
-Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class%%i" /v "RMElpg" /t REG_DWORD /d "00000fff" /f > nul 2>&1
-Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class%%i" /v "RMFspg" /t REG_DWORD /d "0000000f" /f > nul 2>&1
-Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class%%i" /v "RMSlcg" /t REG_DWORD /d "0003ffff" /f > nul 2>&1
